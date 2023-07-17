9980c28e8378529da92b5a0095cea34840b09934
Ali Joveini <alijoveini80@gmail.com>
2023-07-17 21:18:47 +0330

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ["myapp/templates/myapp/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
